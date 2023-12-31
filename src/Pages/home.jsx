4bbd22ddd928883853e961fda307ff875b89222d
khalid-libdeh <khaledabulibdeh@gmail.com>
2023-12-31 17:32:37 +0200

    handle filter

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -10,23 +10,24 @@ import styled from "styled-components";
 const MainContainer = styled(Container)`
     padding-top: 18px;
 `
-let sortOptions = ["Default", "Topic Title","Author Name"]
-let filterOptions = ["Default", "Web Development Languages", "Frontend Frameworks and Libraries","Backend Frameworks and Libraries","Databases and APIs","Web Development Concepts and Technologies"]
+let sortOptions = ["Default", "Topic Title", "Author Name"]
+let filterOptions = ["Default", "Web Development Languages", "Frontend Frameworks and Libraries", "Backend Frameworks and Libraries", "Databases and APIs", "Web Development Concepts and Technologies"]
 
 function Home() {
 
     const [topics, setTopics] = useState(null)
+    const [filteredTopics, setFilteredTopics] = useState(topics)
     const [loading, setLoading] = useState(false)
     const [searchVal, setSearchVal] = useState('');
-    const [sort, setSort] = useState('default')
-    const [filter, setFilter] = useState(null)
-    
+    const [sort, setSort] = useState('Default')
+    const [filter, setFilter] = useState("Default")
+
     useEffect(() => {
         setLoading(true)
         fetchTopics(searchVal).then((res) => {
             setTopics(res.data)
             setLoading(false)
-            
+
         }).catch(err => {
             console.log(err)
             setLoading(false)
@@ -34,19 +35,27 @@ function Home() {
     }, [searchVal]);
 
 
-    useEffect(() =>{
-        console.log(filter)
-    },[filter])
+    useEffect(() => {
+        if (filter === "Default")
+            setFilteredTopics(topics)
+        else
+            setFilteredTopics(
+                topics?.filter((el) => {
+                    return (el.category === filter)
+                })
+            )
+        console.log(filteredTopics)
+    }, [filter, topics])
 
-    useEffect(() =>{
+    useEffect(() => {
         console.log(sort)
-    },[sort])
+    }, [sort])
     return (
         <div>
             <MainContainer>
-                <SearchBar sortOptions={sortOptions} filterOptions={filterOptions} updateSearchVal={setSearchVal} updateSort={setSort} updateFilter={setFilter}/>
-                {loading ? <Loading/> : <CardsGrid topics={topics}/>}
-         
+                <SearchBar sortOptions={sortOptions} filterOptions={filterOptions} updateSearchVal={setSearchVal} updateSort={setSort} updateFilter={setFilter} />
+                {loading ? <Loading /> : <CardsGrid topics={filteredTopics} />}
+
             </MainContainer>
         </div>
     )
