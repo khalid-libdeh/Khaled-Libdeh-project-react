eec568bb8df51ff4af4c0f8751745302668f45c1
khalid-libdeh <khaledabulibdeh@gmail.com>
2024-01-02 17:00:48 +0200

    context

--- src/Layout/layout.jsx
+++ src/Layout/layout.jsx
@@ -2,13 +2,15 @@ import Container from "../Containers/container"
 import Header from "./header"
 import { Outlet } from "react-router-dom"
 import FavContainer from "../Favourites/favContainer"
+import { MyContext } from "./context"
 
 const Layout = () => {
     return (
         <div>
+
             <Header title="Web Topics" titleColor="var(--brand-primary)" />
             <Outlet />
-            <FavContainer/>
+            <FavContainer />
         </div>
     )
 }
